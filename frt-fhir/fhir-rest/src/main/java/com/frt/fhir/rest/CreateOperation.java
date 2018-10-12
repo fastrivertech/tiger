6b7e3ed33e2517508f222f0e9c3c096dc58d1291
orchardhillsdragon <43804807+orchardhillsdragon@users.noreply.github.com>
2018-10-11 17:23:54 -0700

    added CRUD operation classes for future microservices.

---
+++ frt-fhir/fhir-rest/src/main/java/com/frt/fhir/rest/CreateOperation.java
@@ -0,0 +1,15 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright(c) 2018 Fast River Technologies Inc. All Rights Reserved.
+ * 
+ * $Id:					$: Id of last commit                
+ * $Revision:			$: Revision of last commit 
+ * $Author: cye			$: Author of last commit       
+ * $Date:	10-10-2018	$: Date of last commit
+ */
+package com.frt.fhir.rest;
+
+public class CreateOperation {
+
+}
