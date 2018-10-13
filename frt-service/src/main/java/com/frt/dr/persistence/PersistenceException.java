56bdbc40520ac1fdf1818e25e71fe07bbd8a7507
orchardhillsdragon <43804807+orchardhillsdragon@users.noreply.github.com>
2018-10-13 15:59:13 -0700

    added core service module.

---
+++ frt-service/src/main/java/com/frt/dr/persistence/PersistenceException.java
@@ -0,0 +1,55 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2018 Fast River Technologies Inc. All Rights Reserved.
+ * 
+ * $Id:					$: Id of last commit                
+ * $Revision:			$: Revision of last commit 
+ * $Author: cye			$: Author of last commit       
+ * $Date:	10-10-2018	$: Date of last commit
+ */
+package com.frt.dr.persistence;
+
+/**
+ * PersistenceException class
+ * 
+ * @author cqye
+ */
+public class PersistenceException extends Exception {
+
+	/**
+	 * PersistenceException Constructor
+	 */
+	public PersistenceException() {
+		super();
+	}
+
+	/**
+	 * PersistenceException Constructor
+	 * 
+	 * @param m Message string
+	 */
+	public PersistenceException(String m) {
+		super(m);
+	}
+
+	/**
+	 * PersistenceException Constructor
+	 * 
+	 * @param m Message string
+	 * @param t Throwable inherited
+	 */
+	public PersistenceException(String m, Throwable t) {
+		super(m, t);
+	}
+
+	/**
+	 * PersistenceException Constructor
+	 * 
+	 * @param t Throwable inherited
+	 */
+	public PersistenceException(Throwable t) {
+		super(t);
+	}
+
+}
