8c9ce2352747ef4f370fa7bbe1c8b9b0be5044da
orchardhillsdragon <43804807+orchardhillsdragon@users.noreply.github.com>
2018-11-09 15:12:24 -0800

    added JUnit test framework.

---
+++ frt-fhir/fhir-rest/src/test/java/com/frt/fhir/parser/JsonParserTest.java
@@ -0,0 +1,14 @@
+package com.frt.fhir.parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JsonParserTest {
+
+	@Test
+	public void test() {
+		new JsonParser();
+	}
+
+}
