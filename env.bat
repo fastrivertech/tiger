af39fab3371a892125089fe81522a0fc4eb7ddb3
JIMFUQIAN <45013933+JIMFUQIAN@users.noreply.github.com>
2018-11-26 13:10:27 -0800

    check back to original content

--- env.bat
+++ env.bat
@@ -2,13 +2,15 @@ REM Copyright (c) 2018 Fast River Technologies Inc. Irvine, CA, USA
 REM All Rights Reserved.
 REM Product Build Environment
 @echo off
-set JAVA_HOME=C:\JDK10.0.1
-set JRE_HOME=C:\JRE10.0.1
-set ANT_HOME=D:\apache-ant-1.10.4
+set JAVA_HOME=C:\Java\jdk-10.0.2
+set JRE_HOME=C:\Java\jre-10.0.2
+set ANT_HOME=C:\apache\ant\apache-ant-1.10.5
+set MAVEN_HOME=C:\apache\maven\apache-maven-3.5.4
 set MAVEN_HOME=D:\apache-maven-3.5.4
 set PATH=%JAVA_HOME%/bin;%MAVEN_HOME%/bin;%ANT_HOME%/bin;%PATH%
 set ANT_OPTS=-Xmx512m
 set MAVEN_OPTS=-Xmx512m
 
-set CATALINA_HOME=D:\APACHE\apache-tomcat-9.0.8
-set CATALINE_BASE=D:\APACHE\apache-tomcat-9.0.8\base
+set CATALINA_HOME=C:\fastrivertech-dev\apache-tomcat-9.0.12
+set CATALINE_BASE=C:\fastrivertech-dev\apache-tomcat-9.0.12\base
+
