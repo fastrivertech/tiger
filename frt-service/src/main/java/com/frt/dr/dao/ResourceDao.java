625905ce3803e1d0c23cb5500b290d16b59aac3e
JIMFUQIAN <45013933+JIMFUQIAN@users.noreply.github.com>
2019-01-07 23:08:04 -0800

    added missing classes and removed ResourceComponent.java

--- frt-service/src/main/java/com/frt/dr/dao/ResourceDao.java
+++ frt-service/src/main/java/com/frt/dr/dao/ResourceDao.java
@@ -11,10 +11,71 @@
  */
 package com.frt.dr.dao;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityTransaction;
+import javax.persistence.LockTimeoutException;
+import javax.persistence.PersistenceException;
+import javax.persistence.PessimisticLockException;
+import javax.persistence.Query;
+import javax.persistence.QueryTimeoutException;
+import javax.persistence.RollbackException;
+import javax.persistence.TransactionRequiredException;
+
+import com.frt.dr.model.Resource;
+
 /**
  * ResourceDao class
- * @author chaye
+ * @author jfu
  */
-public class ResourceDao {
+public class ResourceDao extends BaseDao<Resource,String> {
+
+	@Override
+	public Optional<Resource> save(Resource entry) throws DaoException {
+		EntityTransaction transaction = null;
+		try {
+			transaction = em.getTransaction();
+			transaction.begin();
+			em.persist(entry);
+			transaction.commit();
+			return Optional.of(entry);
+		} catch (IllegalStateException |
+				 RollbackException ex) {
+			try {
+				if (transaction != null) {
+					transaction.rollback();
+				}
+			} catch (IllegalStateException | RollbackException ignore) {
+			}
+			throw new DaoException(ex);
+		}
+	}
+
+	@Override
+	public Optional<Resource> findById(String id) throws DaoException {
+		try {
+	   	    Query query = em.createNamedQuery("getResourceById");
+            query.setParameter("id", id);
+            List<Resource> resources = (List<Resource>) query.getResultList();          							
+            Optional<Resource> resource = null;
+            if (resources.size()>0) {
+            	
+            	resource = Optional.ofNullable(resources.get(0));
+            }
+            else {
+            	resource = Optional.empty();
+            }
+			return resource;
+		} catch (IllegalArgumentException | 
+				 QueryTimeoutException |
+				 TransactionRequiredException |
+				 PessimisticLockException |
+				 LockTimeoutException ex) {
+			throw new DaoException(ex);			
+		} catch (PersistenceException ex) {
+			throw new DaoException(ex);						
+		}
+	}
 
 }
