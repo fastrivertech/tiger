2d5e5efc7d344cffbe00ca0e52b64dfb03d3ebe2
JIMFUQIAN <45013933+JIMFUQIAN@users.noreply.github.com>
2019-01-10 22:32:53 -0800

    added end 2 end test for patient create / read: com.frt.fhir.rest.end2end.FHIRResourceClientTest

--- frt-fhir/fhir-rest/src/test/java/com/frt/fhir/rest/end2end/FHIRResourceClientTest.java
+++ frt-fhir/fhir-rest/src/test/java/com/frt/fhir/rest/end2end/FHIRResourceClientTest.java
@@ -1,14 +1,14 @@
 package com.frt.fhir.rest.end2end;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static java.nio.file.StandardCopyOption.*;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.CopyOption;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
@@ -17,12 +17,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
-import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
@@ -30,14 +25,6 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.LifecycleState;
-import org.apache.catalina.WebResourceRoot;
-import org.apache.catalina.core.StandardContext;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.DirResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
 import org.hl7.fhir.dstu3.model.Patient;
 import org.junit.After;
 import org.junit.Before;
@@ -51,56 +38,65 @@ import com.frt.fhir.model.base.BaseMapper;
 import ca.uhn.fhir.context.FhirContext;
 
 public class FHIRResourceClientTest {
-	// http://localhost:8080/frt-fhir-rest/1.0/Patient/2
-	public static final String FHIR_RESOURCE_SERVICE_EP = "http://localhost:8088/frt-fhir-rest/1.0/Patient";
-	public static final String WEBAPP_DIR_LOCATION = "./src/test/webapp/";
-	public static final String TOMCAT_BASEDIR_LOCATION = "src/test/";
+	public static final String CATALINA_HOME = System.getenv("CATALINA_HOME");
+	public static final String FHIR_RESOURCE_SERVICE_EP = "http://localhost:8080/frt-fhir-rest/1.0/Patient";
 	public static final String FRT_WAR_LOCATION = "src/../target/frt-fhir-rest.war";
 	public static final String FRT_DERBY_DB_LOCATION = "src/../../../frt-schema/derby/";
 	public static final String CREATE_PT_SQL = FRT_DERBY_DB_LOCATION + "create_patient_tables.sql";
 	public static final String DROP_PT_SQL = FRT_DERBY_DB_LOCATION + "drop_patient_tables.sql";
-	public static final String DERBY_EMBEDDED_DRIVER_NAME = "org.apache.derby.jdbc.EmbeddedDriver";
-	public static final String DERBY_EMBEDDED_DB_URL = "jdbc:derby:" + FRT_DERBY_DB_LOCATION + "TEST_TIGER_DB;create=true";
-	// <property name="javax.persistence.jdbc.url"
-	// value="jdbc:derby:C:\apache\derby\tiger_db"/>
+	public static final String DERBY_NET_DRIVER_NAME = "org.apache.derby.jdbc.ClientDriver";
+	public static final String DERBY_NET_DB_URL = "jdbc:derby://localhost:1527/tiger_db;create=true";
+	public static final String[] TOMCAT_READY_MARK = new String[] {"org.apache.catalina.startup.Catalina.start Server startup in "};
+	public static final String[] DERBY_READY_MARK = new String[] {
+			"Apache Derby Network Server", 
+			"started and ready to accept connections on port"};
 
-	private Client client;
 	private WebTarget webTarget;
-	private ca.uhn.fhir.context.FhirContext context;
+	private boolean setUpOk;
 	private ca.uhn.fhir.parser.JsonParser parser;
-
+	private String testPatientID;
+	
 	@Before
 	public void setUp() throws Exception {
+		setUpOk = true;
+		
+		FhirContext context = FhirContext.forDstu3();
+		parser = (ca.uhn.fhir.parser.JsonParser)context.newJsonParser();
 
-		client = ClientBuilder.newClient();
-		webTarget = client.target(FHIR_RESOURCE_SERVICE_EP);
-		context = FhirContext.forDstu3();
-		parser = (ca.uhn.fhir.parser.JsonParser) context.newJsonParser();
-
-		boolean ret = prepareDatabase();
-
-		if (!ret) {
-			throw new Exception("prepare DB error, test aborted.");
+		webTarget = ClientBuilder.newClient().target(FHIR_RESOURCE_SERVICE_EP);
+		
+		if (CATALINA_HOME==null||CATALINA_HOME.isEmpty()) {
+			setUpOk = false;
+			System.out.println("CATALINA_HOME not set, test not ready, setUpOk=" + setUpOk);
 		}
+		
+		if (setUpOk) {
+			setUpOk = startDerby();
+		}
+		
+		System.out.println("After startDerby()..., setUpOk=" + setUpOk);
+		
+		if (setUpOk) {
+			System.out.println("Before prepareDatabase()..., setUpOk=" + setUpOk);
+			setUpOk = prepareDatabase();
+		}
+		
+		System.out.println("Before deployFRTWebApp()...");
+		
+		deployFRTWebApp();
 
-		// deploy frt-fhir-rest.war to web location
-		deployFRT();
-
-		// launch tomcat in another thread
-		ExecutorService executor = Executors.newFixedThreadPool(2);
-		Future<LifecycleState> future = executor.submit(() -> {
-			return startTomcat();
-		});
-
-		if (!future.get().equals(LifecycleState.STARTED)) {
-			// wait another 10 sec
-			Thread.currentThread().sleep(10000);
+		System.out.println("Before startTomcat()...setUpOk=" + setUpOk);
+		
+		if (setUpOk) {
+			//setUpOk = startTomcat();
+			startTomcat();
+			System.out.println("After startTomcat()...setUpOk=" + setUpOk);
 		}
 	}
 
 	@Test
-	public void test() {
-		// create patient
+	public void test() throws IOException {
+		assertTrue("setUp() did not go through correctly, fail the test.", setUpOk);
 		Response response = createPatient();
 		if (response != null && response.getStatus() == 200) {
 			System.out.println("Patient created, return code: " + response.getStatus());
@@ -111,7 +107,7 @@ public class FHIRResourceClientTest {
 			} else {
 				System.err.println("Patient create error, createPatient() return null");
 			}
-			return;
+			fail("Invoke FRT create Patient RESTFul service error.");
 		}
 		// read the patient back
 		Patient p = readPatient();
@@ -121,29 +117,34 @@ public class FHIRResourceClientTest {
 			ResourceMapperFactory factory = ResourceMapperFactory.getInstance();
 			ResourceMapperInterface mapper = factory.create("Patient");
 			Object target = mapper.from(resourcePair.getFhir()).to(resourcePair.getFrt()).map(p);
-			if (target instanceof com.frt.dr.model.base.Patient) {
-				String frtStr = BaseMapper.resourceToJson((com.frt.dr.model.base.Patient) target);
-				try {
-					String gold = readFromFile("src/test/data/frt_patient_sample_gold.json");
-					System.out.println("frt=" + frtStr);
-					System.out.println("gold=" + gold);
-					assertEquals("FRT Patient json does not match the cannonical json string.", gold, frtStr);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					System.err.println("Gold file not found: src/test/data/frt_patient_sample_gold.json");
-				}
-			} else {
-				System.err.println("End2End test failed: Expect FRT Patient:" + target.getClass().getCanonicalName());
+			assertTrue("End2End test failed: Expect HAPI Patient returned from request.",
+					target instanceof com.frt.dr.model.base.Patient);
+			String frtStr = BaseMapper.resourceToJson((com.frt.dr.model.base.Patient) target);
+			try {
+				String gold = readFromFile("src/test/data/frt_patient_sample_gold.json");
+				System.out.println("frt=" + frtStr);
+				System.out.println("gold=" + gold);
+				assertEquals("FRT Patient json does not match the cannonical json string.", gold, frtStr);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				fail("IOException when read gold file: src/test/data/frt_patient_sample_gold.json");
 			}
-		} else {
-			System.err.println("End2End test failed: Expect HAPI Patient returned from request.");
 		}
 	}
 
 	@After
 	public void tearDown() {
+		shutDownTomcat();
+		shutDownDerby();
+	}
 
+	private void shutDownTomcat() {
+		executeCommand(CATALINA_HOME + File.separator + "bin" + File.separator + "catalina.bat stop", null, 15000l);
+	}
+
+	private void shutDownDerby() {
+		executeCommand(FRT_DERBY_DB_LOCATION + "shutdown-derby.bat", null, 15000l);
 	}
 
 	private boolean prepareDatabase() throws ClassNotFoundException, SQLException, IOException {
@@ -196,10 +197,11 @@ public class FHIRResourceClientTest {
 					line = line.substring(0, idx); // strip comment
 				}
 				// a solid line
-				if ((line.startsWith("CREATE ") || line.startsWith("INSERT ") || line.startsWith("DROP "))&&line.endsWith(";")) {
+				if ((line.startsWith("CREATE ") || line.startsWith("INSERT ") || line.startsWith("DROP "))
+						&& line.endsWith(";")) {
 					// a new SQL statement started
 					int len = line.length();
-					line = line.substring(0, len-1);
+					line = line.substring(0, len - 1);
 					statements.add(line);
 					continue;
 				}
@@ -212,20 +214,19 @@ public class FHIRResourceClientTest {
 					statement.append(line);
 				}
 			}
-			
+
 			if (statement.length() > 0) {
 				statements.add(statement.toString());
 			}
 
-			System.out.println("SQL commands: " + statements.toString());
-			
+//			System.out.println("SQL commands: " + statements.toString());
+//
 			Statement sqlstatement = conn.createStatement();
-			
+
 			for (String s : statements) {
 				try {
 					int ret = sqlstatement.executeUpdate(s);
-				}
-				catch (SQLException e) {
+				} catch (SQLException e) {
 					if (ignoreErr) {
 						continue;
 					}
@@ -242,77 +243,133 @@ public class FHIRResourceClientTest {
 	}
 
 	private Connection createConnection() throws SQLException, ClassNotFoundException {
-		Class.forName(DERBY_EMBEDDED_DRIVER_NAME);
-		return DriverManager.getConnection(DERBY_EMBEDDED_DB_URL);
+		Class.forName(DERBY_NET_DRIVER_NAME);
+		return DriverManager.getConnection(DERBY_NET_DB_URL);
 	}
 
-	private void deployFRT() throws IOException {
+	private void deployFRTWebApp() throws IOException {
 		File war = new File(FRT_WAR_LOCATION);
-		File webAppDir = new File(WEBAPP_DIR_LOCATION);
-		File destWar = new File(webAppDir, "frt-fhir-rest.war");
 		if (!war.exists() || !war.isFile()) {
-			// things do not look right
+			String warPath = war.getAbsolutePath();
 			throw new IllegalStateException(
-					"FRT WAR file not available at location : /target, please built the war file first.");
+					"FRT WAR file not available at location : " + warPath + ", please built the war file first.");
 		}
+		File tomcatDir = new File(CATALINA_HOME);
+		if (!tomcatDir.exists()) {
+			throw new IllegalStateException("Tomcat installation not detected at CATALINA_HOME :" + CATALINA_HOME
+					+ ", Please install tomcat and set CATALINA_HOME accordingly.");
+		}
+		File webAppDir = new File(tomcatDir, "webapps");
 		if (!webAppDir.exists()) {
 			webAppDir.mkdirs();
 		}
+		File destWar = new File(webAppDir, "frt-fhir-rest.war");
 		Files.copy(war.toPath(), destWar.toPath(), REPLACE_EXISTING);
 	}
 
-	private LifecycleState startTomcat() {
-		Tomcat tomcat = new Tomcat();
-		// avoid default 8080 - assume it is in use by user for manual test
-		String webPort = System.getenv("PORT");
-		if (webPort == null || webPort.isEmpty()) {
-			webPort = "8088";
-		}
-
-		tomcat.setPort(Integer.valueOf(webPort));
-		tomcat.setHostname("localhost");
-		String appBase = WEBAPP_DIR_LOCATION;
-		tomcat.getHost().setAppBase(appBase);
-		
-		File docBase = new File(TOMCAT_BASEDIR_LOCATION);
-		Context context = tomcat.addContext("", docBase.getAbsolutePath());
-		
-		try {
-			Thread.currentThread().sleep(20000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return tomcat.getServer().getState();
-	}
-
-	private Response createPatient() {
+	private Response createPatient() throws IOException {
 		// create a HAPI patient
 		File f = new File("src/test/data/fhir_patient_resource_sample_6k.json");
-		FileReader fr = null;
-		try {
-			fr = new FileReader(f);
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			System.err.println("File not found: " + f.getPath());
-			return null;
-		}
-
-		org.hl7.fhir.dstu3.model.Patient hapi = parser.doParseResource(org.hl7.fhir.dstu3.model.Patient.class, fr);
+		String json = new String(Files.readAllBytes(Paths.get("src/test/data/fhir_patient_resource_sample_6k.json")),
+				StandardCharsets.UTF_8);
+		System.out.println("Sample patient resource json file: " + f.getAbsolutePath());
+		System.out.println("Sample patient resource json : " + json);
+		// decode to HAPI Patient
+		Patient p = (Patient)parser.parseResource(json);
+		p.setId((testPatientID="IDFROMEND2ENDTEST"+System.currentTimeMillis()));
 		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
-		return invocationBuilder.post(Entity.entity(hapi, MediaType.APPLICATION_JSON));
+		Response resp = invocationBuilder.post(Entity.json(this.parser.encodeResourceToString(p)));
+		System.out.println("RESP: " + resp.toString());
+		return resp;
 	}
 
 	private Patient readPatient() {
-		WebTarget patientRS = webTarget.path("2");
+		WebTarget patientRS = webTarget.path(testPatientID);
 		Invocation.Builder invocationBuilder = patientRS.request(MediaType.APPLICATION_JSON);
-		return invocationBuilder.get(Patient.class);
+		Patient p = invocationBuilder.get(Patient.class);
+		System.out.println("Patient returned = " + p.getId());
+		return p;
 	}
 
 	private static String readFromFile(String filePath) throws IOException {
 		return new String(Files.readAllBytes(Paths.get(filePath)));
 	}
 
+	private boolean startDerby() {
+		return executeCommand(FRT_DERBY_DB_LOCATION + "start-derby.bat", DERBY_READY_MARK, 20000l);
+	}
+
+	private boolean startTomcat() {
+		return executeCommand(CATALINA_HOME + File.separator + "bin" + File.separator + "catalina.bat run", TOMCAT_READY_MARK, 100000l);
+	}
+
+	private boolean executeCommand(String cmd, String[] ready_mark, long timeout) {
+		boolean startedOK = true;
+		System.out.println(">>>>>>>>>>>>>>>>>>>>Executed command: " + cmd);
+		System.out.flush();
+		try {
+			Process p = Runtime.getRuntime().exec(cmd);
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			// read the output from the command
+			System.out.println("Process's STDOUT:\n");
+			boolean timedOut = readWithTimeout(stdInput, ready_mark, timeout);
+			// read any errors from the attempted command
+			if (timedOut&&ready_mark!=null) {
+				// if process start timed out, drain some std err
+				System.out.println("Process's STDERR:\n");
+				readWithTimeout(stdError, null, timeout);
+				startedOK = false;
+			}
+			System.out.println("Executed command: " + cmd);
+		} catch (IOException e) {
+			System.out.println("Exception when execute: " + cmd);
+			e.printStackTrace();
+			startedOK = false;
+		}
+		System.out.println(">>>>>>>>>>>>>>>>>>>>Executed command: " + cmd + ", return : " + startedOK);
+		System.out.flush();
+		return startedOK;
+	}
+
+	private boolean readWithTimeout(BufferedReader reader, String[] ready_mark, long timeout) throws IOException {
+		boolean timedOut = false;
+		String line = null;
+		long start = System.currentTimeMillis();
+		while (true) {
+			if (System.currentTimeMillis()-start > timeout) {
+				timedOut = true;
+				break;
+			}
+			if (!reader.ready()) {
+				try {
+					Thread.currentThread().sleep(200);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				continue;
+			}
+			line = reader.readLine();
+			if (line!=null&&!line.isEmpty()) {
+				System.out.println(line);
+				if (ready_mark!=null) {
+					int matchCount = ready_mark.length;
+					for (String mark: ready_mark) {
+						if (line.contains(mark)) {
+							matchCount--;
+						}
+					}
+					if (matchCount==0) {
+						System.out.println("READY MARK DETECTED: PROCESS STARTED AND READY.\n");
+						break;
+					}
+				}
+			}
+			else {
+				System.out.print(".");
+			}
+		}
+		return timedOut;
+	}
 }
