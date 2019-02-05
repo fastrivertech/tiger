207562faa74828b0a5a3477f6ac2294b30037a14
orchardhillsdragon <43804807+orchardhillsdragon@users.noreply.github.com>
2019-02-04 22:56:40 -0800

    added the support for FHIR patient history/transaction

---
+++ frt-service/src/main/java/com/frt/dr/model/base/PatientTransaction.java
@@ -0,0 +1,23 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2018 Fast River Technologies Inc. Irvine, CA, USA 
+ * All Rights Reserved.
+ * 
+ * $Id:					$: Id of last commit                
+ * $Revision:			$: Revision of last commit 
+ * $Author: cye			$: Author of last commit       
+ * $Date:	10-10-2018	$: Date of last commit
+ */
+package com.frt.dr.model.base;
+
+import java.io.Serializable;
+
+/**
+ * PatientTransaction class
+ * @author cqye
+ */
+public class PatientTransaction implements Serializable {
+	private static final long serialVersionUID = -8321293485415819089L;
+
+}
