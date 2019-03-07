ab1a6cf71f281c02f7c4812c280433a7eb742223
orchardhillsdragon <43804807+orchardhillsdragon@users.noreply.github.com>
2019-03-07 14:59:06 -0800

    changes for the issue #45: Enhance Fhir Stream RealTime Discovery Prototype

---
+++ frt-stream/src/main/java/com/frt/stream/io/FhirMessageReader.java
@@ -0,0 +1,131 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright(c) 2018 Fast River Technologies Inc. All Rights Reserved.
+ * 
+ * $Id:					$: Id of last commit                
+ * $Revision:			$: Revision of last commit 
+ * $Author: cye			$: Author of last commit       
+ * $Date:	10-10-2018	$: Date of last commit
+ */
+package com.frt.stream.io;
+
+import java.util.Collections;
+import java.util.concurrent.CountDownLatch;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.KafkaException;
+import com.frt.stream.application.ApplicationThread;
+import com.frt.stream.application.ParticipatingApplication;
+import com.frt.stream.application.StreamApplicationException;
+import com.frt.stream.service.StreamServiceConfig;
+import com.frt.stream.service.StreamServiceException;
+
+public class FhirMessageReader implements ParticipatingApplication {
+
+	private Consumer<Long, String> consumer;
+	private StreamServiceConfig config;
+	private ApplicationThread applicationThread;		
+	private CountDownLatch latch;
+
+	public FhirMessageReader() {
+	}
+
+	@Override
+	public void initialize() 
+		throws StreamApplicationException {
+		try {
+			config = StreamServiceConfig.getInstance();											
+			consumer = new KafkaConsumer<>(config.getConsumerConfig());
+			consumer.subscribe(Collections.singletonList(config.get(StreamServiceConfig.STREAM_TOPIC)));
+			
+			System.out.println("fhir message reader connecting to fhir stream [" + 
+					config.get(StreamServiceConfig.STREAM_TOPIC) + 
+					"] on stream broker [" +
+					config.get(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG) + "] ...");
+			
+			applicationThread = new ApplicationThread(this);	
+			
+			latch = new CountDownLatch(1);
+			Runtime.getRuntime().addShutdownHook(new Thread("fhir-message-reader-shutdown-hook") {
+				@Override
+				public void run() {
+					latch.countDown();
+				}
+			});
+			
+		} catch (StreamServiceException ssex) {
+			throw new StreamApplicationException(ssex);
+		}
+	}	
+	
+	@Override
+	public void run() {
+		try {
+			receives();
+		} catch (StreamApplicationException ex) {
+			throw ex;
+		}
+	}
+	
+	public void receives() {
+		int count = 0;
+		while (true) {
+			ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
+			if (consumerRecords.count() == 0) {
+				count++;
+				if (count > 300)
+					break;
+				else
+					continue;
+			}
+			consumerRecords.forEach(record -> {
+				System.out.printf("    received: (%s, %s, %d, %d)\n", 
+						           record.key(), record.value(), record.partition(), record.offset());
+			});
+			consumer.commitAsync();
+		}
+	}
+		
+	public void start() 
+		throws StreamApplicationException {
+		try {			
+			System.out.println("fhir message reader running ...");
+			applicationThread.start();
+			latch.await();
+			close();
+			System.out.println("fhir message reader stopped ...");			
+		} catch (KafkaException | IllegalStateException | InterruptedException ex) {
+			throw new StreamApplicationException(ex);
+		}
+	}
+	
+	@Override
+	public void close() {
+		try {
+			if (applicationThread != null) {
+				applicationThread.close();
+			}			
+			if (consumer != null) {
+				consumer.close();
+			}
+		} catch (KafkaException ex) {
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			FhirMessageReader reader = new FhirMessageReader();
+			reader.initialize();
+			
+			System.out.println("fhir message reader application exit(0)");
+			System.exit(0);			
+		} catch (StreamApplicationException ex) {
+			System.exit(1);
+			System.out.println("fhir message reader application exit(1)");			
+		}
+	}
+	
+}
--- frt-stream/src/main/java/com/frt/stream/io/FhirMessageWriter.java
+++ frt-stream/src/main/java/com/frt/stream/io/FhirMessageWriter.java
@@ -8,45 +8,52 @@
  * $Author: cye			$: Author of last commit       
  * $Date:	10-10-2018	$: Date of last commit
  */
-package com.frt.stream.sample;
+package com.frt.stream.io;
 
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-public class FhirMessageProducer {
+import com.frt.stream.application.ApplicationThread;
+import com.frt.stream.application.ParticipatingApplication;
+import com.frt.stream.application.StreamApplicationException;
+import com.frt.stream.service.StreamServiceConfig;
+import com.frt.stream.service.StreamServiceException;
+
+public class FhirMessageWriter implements ParticipatingApplication {
 
 	private static int id = 0; 
 	private Producer<String, String> producer;
+	private StreamServiceConfig config;
+	private ApplicationThread applicationThread;		
 	
-	public FhirMessageProducer() {
+	public FhirMessageWriter() {
+	}
+	
+	@Override
+	public void initialize() 
+		throws StreamApplicationException {
 		try {
-			Properties props = new Properties();
-//			props.put("acks", "all");
-//			props.put("retries", 0);
-//			props.put("batch.size", 16384);
-//			props.put("linger.ms", 1);
-//			props.put("buffer.memory", 33554432);						
-			String BOOTSTRAP_SERVERS = "10.0.0.16:9092";
-		    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-	        props.put(ProducerConfig.CLIENT_ID_CONFIG, "FhirMessageProducer");
-	        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-	        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-	        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");  
-	        props.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "0000");  
-	 	                
+			config = StreamServiceConfig.getInstance();														
+			Properties props = config.getProducerConfig(); 
 			producer = new KafkaProducer<String, String>(props);
-			producer.initTransactions();
-		} catch (Exception ex) {
-			ex.printStackTrace();
+			producer.initTransactions();			
+		} catch (StreamServiceException ssex) {
+			throw new StreamApplicationException(ssex);
 		}
-		
+	}
+	
+	@Override
+	public void run() {	
 	}
 	
 	public void send(String message) {
@@ -61,18 +68,19 @@ public class FhirMessageProducer {
 		}
 	}
 	
-	protected void finalize()
-		throws Throwable {
+	@Override
+	public void close() {
 		try {
+			if (applicationThread != null) {
+				applicationThread.close();
+			}			
 			if (producer != null) {
 				producer.close();
 			}
-		} finally {
-			super.finalize();
+		} catch (KafkaException ex) {
 		}
 	}
 	
 	public static void main(String[] args) {
-		new FhirMessageProducer().send("test");
 	}
 }
