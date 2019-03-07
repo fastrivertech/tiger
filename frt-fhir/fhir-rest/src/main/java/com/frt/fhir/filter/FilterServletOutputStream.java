96fd333fdcff409da53c1e6a8fa6133799c6f2af
orchardhillsdragon <43804807+orchardhillsdragon@users.noreply.github.com>
2019-03-07 09:38:19 -0800

    refactored load: #42 Synthetic Patient Generation

--- frt-fhir/fhir-rest/src/main/java/com/frt/fhir/filter/FilterServletOutputStream.java
+++ frt-fhir/fhir-rest/src/main/java/com/frt/fhir/filter/FilterServletOutputStream.java
@@ -1,25 +1,35 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright(c) 2018 Fast River Technologies Inc. All Rights Reserved.
+ * 
+ * $Id:					$: Id of last commit                
+ * $Revision:			$: Revision of last commit 
+ * $Author: cye			$: Author of last commit       
+ * $Date:	10-10-2018	$: Date of last commit
+ */
 package com.frt.fhir.filter;
 
 import javax.servlet.*;
 import java.io.*;
 
 public class FilterServletOutputStream extends ServletOutputStream {
-	  private DataOutputStream stream; 
-	 
-	  public FilterServletOutputStream(OutputStream output) { 
-	    stream = new DataOutputStream(output); 
-	  }
-	 
-	  public void write(int b) throws IOException  { 
-	    stream.write(b); 
-	  }
-	 
-	  public void write(byte[] b) throws IOException  { 
-	    stream.write(b); 
-	  }
-	 
-	  public void write(byte[] b, int off, int len) throws IOException  { 
-	    stream.write(b,off,len); 
-	  } 
-	 
+	private DataOutputStream stream;
+
+	public FilterServletOutputStream(OutputStream output) {
+		stream = new DataOutputStream(output);
+	}
+
+	public void write(int b) throws IOException {
+		stream.write(b);
+	}
+
+	public void write(byte[] b) throws IOException {
+		stream.write(b);
+	}
+
+	public void write(byte[] b, int off, int len) throws IOException {
+		stream.write(b, off, len);
+	}
+
 }
