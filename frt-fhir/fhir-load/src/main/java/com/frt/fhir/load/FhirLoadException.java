28c9f0848a57cb9fdf854fc5960783a76de859a2
orchardhillsdragon <43804807+orchardhillsdragon@users.noreply.github.com>
2019-03-07 10:26:27 -0800

    refactored load: #42 Synthetic Patient Generation

--- frt-fhir/fhir-load/src/main/java/com/frt/fhir/load/FhirLoadException.java
+++ frt-fhir/fhir-load/src/main/java/com/frt/fhir/load/FhirLoadException.java
@@ -1,5 +1,47 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright(c) 2018 Fast River Technologies Inc. All Rights Reserved.
+ * 
+ * $Id:					$: Id of last commit                
+ * $Revision:			$: Revision of last commit 
+ * $Author: cye			$: Author of last commit       
+ * $Date:	10-10-2018	$: Date of last commit
+ */
 package com.frt.fhir.load;
 
-public class FhirLoadException {
+public class FhirLoadException extends RuntimeException {
+  
+    /**
+     * FhirLoadException Constructor
+     */
+    public FhirLoadException() {
+        super();
+    }
+
+    /**
+     * FhirLoadException Constructor
+     * @param m Message string
+     */
+    public FhirLoadException(String m) {
+        super(m);
+    }
+
+    /**
+     * FhirLoadException Constructor
+     * @param m Message string
+     * @param t Throwable inherited
+     */
+    public FhirLoadException(String m, Throwable t) {
+        super(m, t);
+    }
+
+    /**
+     * FhirLoadException Constructor
+     * @param t Throwable inherited
+     */
+    public FhirLoadException(Throwable t) {
+        super(t);
+    }
 
 }
