880bf52cd40f1d7869c27d84d53248a100185aac
orchardhillsdragon <43804807+orchardhillsdragon@users.noreply.github.com>
2019-03-07 23:08:02 -0800

    changes for the issue #45: Enhance Fhir Stream RealTime Discovery Prototype

--- frt-script/fhir-stream-writer.bat
+++ frt-script/fhir-stream-writer.bat
@@ -1,3 +1,3 @@
 REM Copyright(c) 2018 Fast River Technologies Inc. All Rights Reserved. 
 @echo off
-%JAVA_HOME%\bin\java -classpath ".\lib\*" com.frt.stream.io.FhirStreamWriter
+%JAVA_HOME%\bin\java -classpath "..\lib\*" com.frt.stream.io.FhirStreamWriter %1
