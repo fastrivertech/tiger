7b3e8f4da2663cf25fa3422d62a2c391a1422da8
JIMFUQIAN <45013933+JIMFUQIAN@users.noreply.github.com>
2019-03-08 22:22:56 -0800

    fix dl batch script classpath due to jersey1.9 jars now in lib/jersey1.9 sub-dir of frt service pack.

--- frt-script/datalake-ingestion.bat
+++ frt-script/datalake-ingestion.bat
@@ -1,3 +1,3 @@
 REM Copyright(c) 2018 Fast River Technologies Inc. All Rights Reserved. 
 @echo off
-%JAVA_HOME%\bin\java -classpath "..\lib\*" com.frt.stream.application.DataLakeIngestion
+%JAVA_HOME%\bin\java -classpath "..\lib\jersey1.9\*;..\lib\*" com.frt.stream.application.DataLakeIngestion
