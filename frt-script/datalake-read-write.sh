80be64a6b2f397e4e9134fe74964cbf6ee6ba2e0
JIMFUQIAN <45013933+JIMFUQIAN@users.noreply.github.com>
2019-03-20 21:25:52 -0700

    added bash scripts for dl injection fhir stream batch write / and continuous read.

---
+++ frt-script/datalake-read-write.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+## Copyright(c) 2018 Fast River Technologies Inc. All Rights Reserved. 
+## @echo off
+
+source ../setEnv.sh
+exec ./showEnv.sh
+
+$JAVA_HOME/bin/java -classpath "..\lib\jersey1.9\*:..\lib\*" com.frt.dl.service.DataLakeService $1 $2
---
+++ frt-script/fhir-stream-writer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+## Copyright(c) 2018 Fast River Technologies Inc. All Rights Reserved. 
+## @echo off
+
+source ../setEnv.sh
+exec ./showEnv.sh
+
+$JAVA_HOME/bin/java -classpath "../lib/*" com.frt.stream.io.FhirStreamWriter $1
