df3f90044645072f05b018c6a227581482a8abcd
orchardhillsdragon <43804807+orchardhillsdragon@users.noreply.github.com>
2019-03-23 22:03:52 -0700

    Reviewed the changes of revision 577.

--- frt-stream/scripts/start.sh
+++ frt-stream/scripts/start.sh
@@ -4,10 +4,10 @@ source ../env.sh
 
 ## start a single zookeeper server
 echo "exec zk==================="
-$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties ;
+$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
 
 ## start a single kafka server
 echo "exec kafka==================="
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
 
 
