f126679c7aeacde46574cee1a40607f4c0c567e0
orchardhillsdragon <43804807+orchardhillsdragon@users.noreply.github.com>
2019-04-01 17:52:26 -0700

    added fhir bundle extract tool in load

---
+++ frt-fhir/fhir-load/scripts/extract.bat
@@ -0,0 +1,4 @@
+REM Copyright (c) 2018 Fast River Technologies Inc. Irvine, CA, USA 
+REM All Rights Reserved.
+REM @echo off
+%JAVA_HOME%\bin\java -cp "../lib/loader/db;../lib/load/db/*;../lib/load/*;../lib/*" com.frt.fhir.load.FhirBundleExtract
