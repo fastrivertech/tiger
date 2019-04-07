7e2bc68b118a65a64ebdf8ad0576e1e16f64b798
orchardhillsdragon <43804807+orchardhillsdragon@users.noreply.github.com>
2019-04-06 21:51:55 -0700

    add scripts and configurations for the charts of the patient group-by organization

--- frt-demo/fhir-stream/confluent/bin/drop-fhir-topics.sh
+++ frt-demo/fhir-stream/confluent/bin/drop-fhir-topics.sh
@@ -1,4 +1,4 @@
-./kafka-topics --delete --zookeeper localhost:2181 --topic FHIR_GROUPBY_ORG_LINK
+./kafka-topics --delete --zookeeper localhost:2181 --topic FHIR_GROUPBY_ORG_SINK
 ./kafka-topics --delete --zookeeper localhost:2181 --topic FHIR_GROUPBY_ORG
 ./kafka-topics --delete --zookeeper localhost:2181 --topic FHIR_AVRO_STREAM
 ./kafka-topics --delete --zookeeper localhost:2181 --topic FhirTopic
