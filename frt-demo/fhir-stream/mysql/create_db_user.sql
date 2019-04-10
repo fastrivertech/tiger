db43c404f6ff248cbce44b515f8d2dd24a4925f7
JIMFUQIAN <45013933+JIMFUQIAN@users.noreply.github.com>
2019-04-10 12:08:01 -0700

    essage

--- frt-demo/fhir-stream/mysql/create_db_user.sql
+++ frt-demo/fhir-stream/mysql/create_db_user.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE FHIR_DB;
-CREATE USER 'frt'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+CREATE USER 'frt'@'%' IDENTIFIED WITH mysql_native_password BY 'F@stR1ver@USA';
 GRANT ALL PRIVILEGES ON *.* TO 'frt'@'%';
