304f88e9571068b99391457b415ba8f77d06cc24
orchardhillsdragon <43804807+orchardhillsdragon@users.noreply.github.com>
2019-04-09 23:13:24 -0700

    added the dashboard of patient group by practitioner

--- frt-demo/fhir-stream/confluent/bin/drop-fhir-topics.sh
+++ frt-demo/fhir-stream/confluent/bin/drop-fhir-topics.sh
@@ -1,6 +1,8 @@
 ./kafka-topics --delete --zookeeper localhost:2181 --topic FHIR_GROUPBY_STATE_SINK
 ./kafka-topics --delete --zookeeper localhost:2181 --topic FHIR_GROUPBY_STATE
 
+./kafka-topics --delete --zookeeper localhost:2181 --topic FHIR_GROUPBY_GP
+
 ./kafka-topics --delete --zookeeper localhost:2181 --topic FHIR_GROUPBY_ORG_SINK
 ./kafka-topics --delete --zookeeper localhost:2181 --topic FHIR_GROUPBY_ORG
 ./kafka-topics --delete --zookeeper localhost:2181 --topic FHIR_AVRO_STREAM
