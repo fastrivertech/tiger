917fb75bdae11e8dc086edde868abb06c65e199b
JIMFUQIAN <45013933+JIMFUQIAN@users.noreply.github.com>
2019-04-23 11:19:27 -0700

    2nd round refactor from streams to dashboards, adopt pie charts queries across the dashboards.

--- frt-demo/fhir-stream/confluent/bin/start-fhir-mysql-sink.sh
+++ frt-demo/fhir-stream/confluent/bin/start-fhir-mysql-sink.sh
@@ -2,10 +2,7 @@ export CLASSPATH="/home/ec2-user/confluent-5.2.0/share/java/kafka-connect-jdbc/*
 export CONFLUENT_HOME=$HOME/confluent-5.2.0
 ./connect-standalone \
 $CONFLUENT_HOME/etc/schema-registry/fhir-connect-avro-standalone.properties \
-$CONFLUENT_HOME/etc/kafka-connect-jdbc/fhir-groupby-org-gender-mysql-sink.properties \
 $CONFLUENT_HOME/etc/kafka-connect-jdbc/fhir-groupby-org-mysql-sink.properties \
-$CONFLUENT_HOME/etc/kafka-connect-jdbc/fhir-groupby-gp-gender-mysql-sink.properties \
 $CONFLUENT_HOME/etc/kafka-connect-jdbc/fhir-groupby-gp-mysql-sink.properties \
-$CONFLUENT_HOME/etc/kafka-connect-jdbc/fhir-groupby-state-gender-mysql-sink.properties \
 $CONFLUENT_HOME/etc/kafka-connect-jdbc/fhir-groupby-state-mysql-sink.properties \
 $CONFLUENT_HOME/etc/kafka-connect-jdbc/fhir-groupby-age-mysql-sink.properties
