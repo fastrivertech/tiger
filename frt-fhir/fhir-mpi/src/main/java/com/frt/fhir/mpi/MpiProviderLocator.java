853d53a68ebd9c7d44adcdaa1799ff8a9ba98d8d
Charles YE <43804807+orchardhillsdragon@users.noreply.github.com>
2019-08-15 16:50:13 -0700

    add fhir merge and link support.

--- frt-fhir/fhir-mpi/src/main/java/com/frt/fhir/mpi/MpiProviderLocator.java
+++ frt-fhir/fhir-mpi/src/main/java/com/frt/fhir/mpi/MpiProviderLocator.java
@@ -7,10 +7,21 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Properties;
+import com.sun.mdm.index.webservice.client.PatientEJB;
+import com.sun.mdm.index.webservice.client.PatientEJBService;
+
 
 public class MpiProviderLocator {
 
 	private Properties props;
+	private static MpiProviderLocator instance;
+	
+	public static MpiProviderLocator getInstance() {
+		if (instance == null) {
+			instance = new MpiProviderLocator();
+		}
+		return instance;
+	}
 	
 	public void config() 
 		throws MpiProviderException {
@@ -22,4 +33,8 @@ public class MpiProviderLocator {
 		}
 	}
 	
+	public PatientEJB mpi() {
+		 PatientEJBService service = new PatientEJBService();
+		 return service.getPatientEJBPort();
+	}
 }
