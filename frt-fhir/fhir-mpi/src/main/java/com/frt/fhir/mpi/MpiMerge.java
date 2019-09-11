cd91f0fbdf2774372890e3ac595d9c64f736e1dd
Charles YE <43804807+orchardhillsdragon@users.noreply.github.com>
2019-09-11 00:02:09 -0700

    add fhir merge and link support.

--- frt-fhir/fhir-mpi/src/main/java/com/frt/fhir/mpi/MpiMerge.java
+++ frt-fhir/fhir-mpi/src/main/java/com/frt/fhir/mpi/MpiMerge.java
@@ -1,8 +1,10 @@
 package com.frt.fhir.mpi;
 
 import java.util.List;
+import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.Patient;
 import org.hl7.fhir.r4.model.Patient.PatientLinkComponent;
+import org.hl7.fhir.r4.model.Patient.LinkType;
 import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Identifier.IdentifierUse;
 import org.hl7.fhir.r4.model.Address;
@@ -10,39 +12,46 @@ import org.hl7.fhir.r4.model.Address.AddressUse;
 import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.r4.model.HumanName.NameUse;
 
-
 public class MpiMerge {
 
 	public static Patient execute(Patient source, Patient  target) {
 		
-		PatientLinkComponent sourceLink = new PatientLinkComponent();
-		sourceLink.setId(source.getId());
+	    PatientLinkComponent sourceLink = new PatientLinkComponent();
+		sourceLink.setType(LinkType.REPLACES);
+		Reference replaces = new Reference();
+		replaces.setReference(source.getId());
+		sourceLink.setOther(replaces);
 		target.addLink(sourceLink);
 
 		PatientLinkComponent targetLink = new PatientLinkComponent();
+		targetLink.setType(LinkType.REPLACEDBY);
+		Reference replaced = new Reference();
+		replaced.setReference(target.getId());
+		targetLink.setOther(replaced);
 		source.addLink(targetLink);
-		source.setActive(false);
-
+			 
 		List<Identifier> identifiers = source.getIdentifier();
 		identifiers.forEach(identifier->{
-			if (identifier.getSystem().contains(("localid"))) {
-				identifier.setUse(IdentifierUse.OLD);
+			Identifier instance = identifier.copy();			
+			if (instance.getSystem().contains(("localid"))) {
+				instance.setUse(IdentifierUse.OLD);
 			}
-			target.addIdentifier(identifier);
-		});
-		
+			target.addIdentifier(instance);
+		});		
 		List<Address> addresses = source.getAddress();
 		addresses.forEach(address->{
-			address.setUse(AddressUse.OLD);
-			target.addAddress(address);
-		});
-		
+			Address instance = address.copy();
+			instance.setUse(AddressUse.OLD);
+			target.addAddress(instance);
+		});		
 		List<HumanName> names = source.getName();
 		names.forEach(name->{
-			name.setUse(NameUse.OLD);
-			target.addName(name);
+			HumanName instance = name.copy();
+			instance.setUse(NameUse.OLD);
+			target.addName(instance);
 		});
 		
+		source.setActive(false);
 		
 		return target;
 	}
